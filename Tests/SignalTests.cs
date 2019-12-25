2e0c05c6561fbe8a17aebc5fca2d3578a9c73de1
Johannes Deml <public@deml.io>
2019-12-25 22:59:24 +0100

    Add basic signal tests

---
+++ Tests/SignalTests.cs
@@ -0,0 +1,162 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SignalTests.cs" company="Supyrb">
+//   Copyright (c) 2019 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Supyrb
+{
+	[Category("Signals")]
+	public class SignalTests
+	{
+		private TestSignal testSignal;
+		private readonly List<string> callLog = new List<string>();
+
+		[SetUp]
+		public void Setup()
+		{
+			callLog.Clear();
+			Signals.Clear();
+			Signals.Get(out testSignal);
+		}
+
+		public void Reset()
+		{
+			callLog.Clear();
+			testSignal.Clear();
+		}
+
+		[Test]
+		public void ListenerOrderTest()
+		{
+			testSignal.AddListener(OnListenerA, -10);
+			testSignal.AddListener(OnListenerB, 0);
+			testSignal.AddListener(OnListenerC, 10);
+
+			testSignal.Dispatch();
+
+			Assert.IsTrue(callLog[0] == "A");
+			Assert.IsTrue(callLog[1] == "B");
+			Assert.IsTrue(callLog[2] == "C");
+			Assert.IsTrue(testSignal.ListenerCount == 3);
+			Reset();
+		}
+		
+		[Test]
+		public void RemoveListenerTest()
+		{
+			testSignal.AddListener(OnListenerA, -10);
+			testSignal.AddListener(OnListenerB, 0);
+			testSignal.AddListener(OnListenerC, 10);
+
+			testSignal.RemoveListener(OnListenerA);
+			testSignal.Dispatch();
+			testSignal.RemoveListener(OnListenerB);
+			testSignal.Dispatch();
+
+			Assert.IsTrue(callLog[0] == "B");
+			Assert.IsTrue(callLog[1] == "C");
+			Assert.IsTrue(callLog[2] == "C");
+			Assert.IsTrue(testSignal.ListenerCount == 1);
+			Reset();
+		}
+
+		[Test]
+		public void PauseContinueSignalTest()
+		{
+			testSignal.AddListener(OnListenerA, -10);
+			testSignal.AddListener(OnPauseListener, 0);
+			testSignal.AddListener(OnListenerB, 10);
+
+			testSignal.Dispatch();
+
+			Assert.IsTrue(callLog.Contains("A"));
+			Assert.IsFalse(callLog.Contains("B"));
+			
+			ContinueSignal();
+			
+			Assert.IsTrue(callLog.Contains("B"));
+			
+			Reset();
+		}
+		
+		[Test]
+		public void ConsumeSignalTest()
+		{
+			testSignal.AddListener(OnListenerA, -10);
+			testSignal.AddListener(OnConsumeListener, 0);
+			testSignal.AddListener(OnListenerB, 10);
+
+			testSignal.Dispatch();
+
+			Assert.IsTrue(callLog.Contains("A"));
+			Assert.IsFalse(callLog.Contains("B"));
+			Reset();
+		}
+		
+		[Test]
+		public void RemoveListenerWhileDispatching()
+		{
+			testSignal.AddListener(OnListenerA, -10);
+			testSignal.AddListener(OnRemoveSelfListener, 0);
+			testSignal.AddListener(OnListenerB, 10);
+
+			testSignal.Dispatch();
+
+			Assert.IsTrue(callLog.Contains("B"));
+			Assert.IsTrue(testSignal.ListenerCount == 2);
+			Reset();
+		}
+		
+		private void OnListenerA()
+		{
+			callLog.Add("A");
+		}
+
+		private void OnListenerB()
+		{
+			callLog.Add("B");
+		}
+		
+		private void OnListenerC()
+		{
+			callLog.Add("C");
+		}
+
+		private void OnListenerGeneric(string logName)
+		{
+			callLog.Add(logName);
+		}
+		
+		private void OnRemoveSelfListener()
+		{
+			callLog.Add("Remove SemoveSelfListener");
+			testSignal.RemoveListener(OnRemoveSelfListener);
+		}
+
+		private void OnConsumeListener()
+		{
+			callLog.Add("Consume Signal");
+			testSignal.Consume();
+		}
+
+		private void OnPauseListener()
+		{
+			callLog.Add("Pause Signal");
+			testSignal.Pause();
+		}
+
+		private void ContinueSignal()
+		{
+			callLog.Add("Continue Signal");
+			testSignal.Continue();
+		}
+	}
+}
