7346fc0f950dece5ab1106b4cb344d4b6b15b2d0
Johannes Deml <public@deml.io>
2020-01-07 08:34:59 +0100

    Add simple detail view and update signal list button to editor

--- Scripts/Editor/SignalsTreeView.cs
+++ Scripts/Editor/SignalsTreeView.cs
@@ -8,6 +8,7 @@
 // </author>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.Collections.Generic;
 using UnityEditor.IMGUI.Controls;
 
@@ -15,23 +16,29 @@ namespace Supyrb
 {
 	internal class SignalsTreeView : TreeView
 	{
+		public delegate void SelectionChangedDelegate(SerializableSystemType selectedType);
+
+		public event SelectionChangedDelegate OnSelectionChanged;
+		
 		private List<TreeViewItem> signals;
+		private List<SerializableSystemType> signalTypes;
+		
 		public SignalsTreeView(TreeViewState treeViewState)
 			: base(treeViewState)
 		{
 			signals = new List<TreeViewItem>();
+			signalTypes = SignalsEditorDatabase.Instance.SignalTypes;
 			Reload();
 		}
 
 		protected override TreeViewItem BuildRoot()
 		{
-			var signalTypes = SignalsEditorDatabase.Instance.SignalTypes;
 			signals.Clear();
-			var root = new TreeViewItem {id = 0, depth = -1, displayName = "Root"};
+			var root = new TreeViewItem {id = -1, depth = -1, displayName = "Root"};
 			for (int i = 0; i < signalTypes.Count; i++)
 			{
 				var signalType = signalTypes[i];
-				signals.Add(new TreeViewItem(i+1, 0, signalType.Name));
+				signals.Add(new TreeViewItem(i, 0, signalType.Name));
 			}
 
 			// Utility method that initializes the TreeViewItem.children and -parent for all items.
@@ -39,6 +46,29 @@ namespace Supyrb
 
 			// Return root of the tree
 			return root;
+		}
+
+		public void UpdateSignalData()
+		{
+			SignalsEditorDatabase.Instance.UpdateDatabase();
+			this.Reload();
+		}
+		
+		protected override void SelectionChanged(IList<int> selectedIds)
+		{
+			base.SelectionChanged(selectedIds);
+			if (OnSelectionChanged == null)
+			{
+				return;
+			}
+			if (selectedIds.Count == 0)
+			{
+				OnSelectionChanged(null);
+			}
+			else
+			{
+				OnSelectionChanged(signalTypes[selectedIds[0]]);
+			}
 		}
 	}
 }
--- Scripts/Editor/SignalsTreeViewWindow.cs
+++ Scripts/Editor/SignalsTreeViewWindow.cs
@@ -1,81 +1,163 @@
-using UnityEditor;
-using UnityEditor.Graphs;
-using UnityEngine;
-using UnityEditor.IMGUI.Controls;
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="SignalsTreeViewWindow.cs" company="Supyrb">
+//   Copyright (c) 2020 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   public@deml.io
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
 
+using UnityEditor;
+using UnityEditor.IMGUI.Controls;
+using UnityEngine;
 
 namespace Supyrb
 {
-	class SignalsTreeViewWindow : EditorWindow
+	internal class SignalsTreeViewWindow : EditorWindow
 	{
-		[SerializeField] 
+		[SerializeField]
 		private TreeViewState treeViewState;
-		
+
 		private SignalsTreeView treeView;
 		private SearchField searchField;
+		private SerializableSystemType currentSelection;
+		private ASignal cachedSignal;
 
-		private const float hierarchyWidth = 250f;
+		private const float HierarchyWidth = 250f;
+		private const float ToolbarHeight = 18f;
+		private const float FooterHeight = 26f;
 
 		private static class Styles
 		{
 			public static GUIStyle HierarchyStyle = (GUIStyle) "OL box";
+			public static GUIStyle FooterStyle = (GUIStyle) "ProjectBrowserBottomBarBg";
 		}
 
-		void OnEnable ()
+		private void OnEnable()
 		{
 			// Check if we already had a serialized view state (state 
 			// that survived assembly reloading)
 			if (treeViewState == null)
-				treeViewState = new TreeViewState ();
+			{
+				treeViewState = new TreeViewState();
+			}
 
 			treeView = new SignalsTreeView(treeViewState);
-			searchField = new SearchField ();
+			treeView.OnSelectionChanged += OnSelectionChanged;
+			searchField = new SearchField();
 			searchField.downOrUpArrowKeyPressed += treeView.SetFocusAndEnsureSelectedItem;
 		}
 
-		void OnGUI ()
+		private void OnGUI()
 		{
-			var contentHeight = this.position.height - EditorGUIUtility.singleLineHeight;
-			
-			DoToolbar ();
-			EditorGUILayout.BeginHorizontal();
+			var contentHeight = this.position.height - ToolbarHeight - FooterHeight;
 
-			
-			EditorGUILayout.BeginVertical(Styles.HierarchyStyle, GUILayout.Width(hierarchyWidth));
-			Rect rect = GUILayoutUtility.GetRect(0, hierarchyWidth, 0, 100000);
+			DoToolbar();
+			EditorGUILayout.BeginHorizontal(GUILayout.Height(contentHeight));
+
+
+			EditorGUILayout.BeginVertical(Styles.HierarchyStyle, GUILayout.Width(HierarchyWidth));
+			var rect = GUILayoutUtility.GetRect(0, HierarchyWidth, 0, 100000);
 			DoTreeView(rect);
 			EditorGUILayout.EndVertical();
-			
-			EditorGUILayout.Separator();
-			
-			GUILayout.Label("Content");
+
+			EditorGUILayout.BeginVertical();
+			DoDetailView();
+			EditorGUILayout.EndVertical();
 
 			EditorGUILayout.EndHorizontal();
-			
+
+			DoFooter();
 		}
 
-		void DoToolbar()
+		private void DoToolbar()
 		{
-			GUILayout.BeginHorizontal (EditorStyles.toolbar);
-			GUILayout.Space (100);
+			GUILayout.BeginHorizontal(EditorStyles.toolbar, 
+				GUILayout.Width(this.position.width), GUILayout.Height(ToolbarHeight));
+			GUILayout.Space(100);
 			GUILayout.FlexibleSpace();
-			treeView.searchString = searchField.OnToolbarGUI (treeView.searchString);
+			treeView.searchString = searchField.OnToolbarGUI(treeView.searchString);
 			GUILayout.EndHorizontal();
 		}
 
-		void DoTreeView(Rect rect)
+		private void DoTreeView(Rect rect)
 		{
 			treeView.OnGUI(rect);
 		}
 
+		private void DoDetailView()
+		{
+			if (currentSelection == null)
+			{
+				GUILayout.Label("Nothing selected");
+				return;
+			}
+
+			if (cachedSignal == null)
+			{
+				cachedSignal = Signals.Get(currentSelection.SystemType) as ASignal;
+			}
+
+			if (cachedSignal == null)
+			{
+				GUILayout.Label("Only signals derived from ASignal supported");
+				return;
+			}
+
+			GUILayout.BeginVertical();
+
+			GUILayout.Label(string.Format("{0} ({1} Listeners)",
+				currentSelection.Name,
+				cachedSignal.ListenerCount));
+			var type = currentSelection.SystemType;
+			var baseType = type.BaseType;
+
+			if (baseType != null)
+			{
+				var genericArguments = baseType.GetGenericArguments();
+				for (var i = 0; i < genericArguments.Length; i++)
+				{
+					var argument = genericArguments[i];
+					GUILayout.Label(argument.Name);
+				}
+			}
+			
+			GUILayout.EndVertical();
+		}
+
+		private void DoFooter()
+		{
+			GUILayout.BeginHorizontal(Styles.FooterStyle, 
+				GUILayout.Width(this.position.width), GUILayout.Height(FooterHeight));
+			GUILayout.Space(100);
+			GUILayout.FlexibleSpace();
+			if (GUILayout.Button("Update Signal List"))
+			{
+				treeView.UpdateSignalData();
+			}
+			GUILayout.EndHorizontal();
+		}
+
+		
 		// Add menu named "My Window" to the Window menu
-		[MenuItem ("Supyrb/Simple Tree Window")]
-		static void ShowWindow ()
+		[MenuItem("Supyrb/Simple Tree Window")]
+		private static void ShowWindow()
 		{
 			// Get existing open window or if none, make a new one:
-			var window = GetWindow<SignalsTreeViewWindow> ();
-			window.titleContent = new GUIContent ("Signals");
-			window.Show ();
+			var window = GetWindow<SignalsTreeViewWindow>();
+			window.titleContent = new GUIContent("Signals");
+			window.Show();
+		}
+
+		private void OnSelectionChanged(SerializableSystemType selectedtype)
+		{
+			currentSelection = selectedtype;
+
+			if (currentSelection != null)
+			{
+				cachedSignal = Signals.Get(currentSelection.SystemType) as ASignal;
+			}
 		}
 	}
 }
