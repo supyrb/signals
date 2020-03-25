88de31443fd95d5ec59c600daa9a90c9fcb48623
Johannes Deml <public@deml.io>
2020-03-25 14:38:56 +0100

    Change move general editor functions to editor utilities

--- Scripts/Editor/SignalsEditorUtilities.cs
+++ Scripts/Editor/SignalsEditorUtilities.cs
@@ -1,5 +1,5 @@
 // --------------------------------------------------------------------------------------------------------------------
-// <copyright file="SignalsEditorAssetUtilities.cs" company="Supyrb">
+// <copyright file="SignalsEditorUtilities.cs" company="Supyrb">
 //   Copyright (c) 2020 Supyrb. All rights reserved.
 // </copyright>
 // <author>
@@ -8,13 +8,15 @@
 // </author>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
 using System.IO;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace Supyrb
 {
-	public static class SignalsEditorAssetUtilities
+	public static class SignalsEditorUtilities
 	{
 		/// <summary>
 		/// Get or Create a ScriptabelObject in the EditorDefaultResources folder.
@@ -64,6 +66,182 @@ namespace Supyrb
 			}
 
 			return asset;
+		}
+		
+		/// <summary>
+		/// Creates a new Texture2D with 1x1 pixels and a defined color.
+		/// Easy solution for editor style background textures that should just be a color
+		/// </summary>
+		/// <param name="col">The color for the new texture</param>
+		/// <returns>A new 1x1 texture with the defined color</returns>
+		public static Texture2D CreateColorTexture(Color col)
+		{
+			Texture2D result = new Texture2D(1, 1);
+			result.SetPixel(0, 0, col);
+			result.Apply();
+
+			return result;
+		}
+		
+		/// <summary>
+		/// Modify the color of a 1x1 texture
+		/// </summary>
+		/// <param name="tex">Texture to modify</param>
+		/// <param name="col">New color</param>
+		public static void ChangeColorTexture(Texture2D tex, Color col)
+		{
+			Assert.IsTrue(tex.width == 1);
+			Assert.IsTrue(tex.height == 1);
+			
+			tex.SetPixel(0, 0, col);
+			tex.Apply();
+		}
+
+		/// <summary>
+		/// Similar to <see cref="EditorGUILayout.ObjectField(UnityEngine.Object,System.Type,UnityEngine.GUILayoutOption[])"/>, but more flexible.
+		/// Draws an editor field fitting to the type and returns the user value for that field.
+		/// Supports
+		/// * <see cref="UnityEngine.Object"/>
+		/// * <see cref="AnimationCurve"/>
+		/// * <see cref="Gradient"/>
+		/// * <see cref="string"/>
+		/// * <see cref="Enum"/>
+		/// * <see cref="bool"/>
+		/// * <see cref="int"/>
+		/// * <see cref="long"/>
+		/// * <see cref="float"/>
+		/// * <see cref="UnityEngine.Vector2"/>
+		/// * <see cref="UnityEngine.Vector3"/>
+		/// * <see cref="UnityEngine.Vector4"/>
+		/// * <see cref="UnityEngine.Color"/>
+		/// </summary>
+		/// <param name="label">Optional label in front of the field</param>
+		/// <param name="type">Type of the value</param>
+		/// <param name="value">Current value for that field</param>
+		/// <returns>The new value set by the user</returns>
+		public static object DrawFittingEditorField(string label, Type type, object value)
+		{
+			if (typeof(UnityEngine.Object).IsAssignableFrom(type))
+			{
+				return EditorGUILayout.ObjectField(label, (UnityEngine.Object) value, type, true);
+			}
+
+			if (type == typeof(AnimationCurve))
+			{
+				return EditorGUILayout.CurveField(label, (AnimationCurve) value);
+			}
+
+			if (type == typeof(Gradient))
+			{
+				return EditorGUILayout.GradientField(label, (Gradient) value);
+			}
+
+			if (type == typeof(string))
+			{
+				return EditorGUILayout.TextField(label, (string) value);
+			}
+
+			if (type.IsEnum)
+			{
+				int enumValue = 0;
+				if (value != null)
+				{
+					enumValue = (int) value;
+				}
+
+				return EditorGUILayout.EnumPopup(label, (Enum) Enum.ToObject(type, enumValue));
+			}
+
+			if (type == typeof(bool))
+			{
+				var boolValue = false;
+				if (value != null)
+				{
+					boolValue = (bool) value;
+				}
+
+				return EditorGUILayout.Toggle(label, boolValue);
+			}
+
+			if (type == typeof(int))
+			{
+				var intValue = 0;
+				if (value != null)
+				{
+					intValue = (int) value;
+				}
+
+				return EditorGUILayout.IntField(label, (int) intValue);
+			}
+
+			if (type == typeof(long))
+			{
+				var longValue = 0L;
+				if (value != null)
+				{
+					longValue = (long) value;
+				}
+
+				return EditorGUILayout.LongField(label, longValue);
+			}
+
+			if (type == typeof(float))
+			{
+				var floatValue = 0f;
+				if (value != null)
+				{
+					floatValue = (float) value;
+				}
+
+				return EditorGUILayout.FloatField(label, floatValue);
+			}
+
+			if (type == typeof(Vector2))
+			{
+				var vectorValue = Vector2.zero;
+				if (value != null)
+				{
+					vectorValue = (Vector2) value;
+				}
+
+				return EditorGUILayout.Vector2Field(label, vectorValue);
+			}
+
+			if (type == typeof(Vector3))
+			{
+				var vectorValue = Vector3.zero;
+				if (value != null)
+				{
+					vectorValue = (Vector3) value;
+				}
+
+				return EditorGUILayout.Vector3Field(label, vectorValue);
+			}
+
+			if (type == typeof(Vector4))
+			{
+				var vectorValue = Vector4.zero;
+				if (value != null)
+				{
+					vectorValue = (Vector4) value;
+				}
+
+				return EditorGUILayout.Vector4Field(label, vectorValue);
+			}
+
+			if (type == typeof(Color))
+			{
+				var colorValue = Color.black;
+				if (value != null)
+				{
+					colorValue = (Color) value;
+				}
+
+				return EditorGUILayout.ColorField(label, colorValue);
+			}
+
+			GUILayout.Label(string.Format("Field Type {0} not supported", type.FullName));
+			return null;
 		}
 	}
 }
