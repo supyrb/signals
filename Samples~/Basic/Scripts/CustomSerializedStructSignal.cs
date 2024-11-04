23b82cc89da94ce8b27068d375e7ab2ec6e52d94
Johannes Deml <public@deml.io>
2024-11-04 20:45:50 +0100

    Add signal with struct as parameter

---
+++ Samples~/Basic/Scripts/CustomSerializedStructSignal.cs
@@ -0,0 +1,23 @@
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ExampleSignal.cs" company="Supyrb">
+//   Copyright (c) 2019 Supyrb. All rights reserved.
+// </copyright>
+// <author>
+//   Johannes Deml
+//   send@johannesdeml.com
+// </author>
+// --------------------------------------------------------------------------------------------------------------------
+
+
+namespace Supyrb
+{
+	public struct CustomStruct
+	{
+		public int Integer;
+		public string String;
+	}
+
+	public class CustomSerializedStructSignal : Signal<CustomStruct>
+	{
+	}
+}
